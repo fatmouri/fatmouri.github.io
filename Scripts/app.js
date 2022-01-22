995791f3f9ac58b59e9d26e4d3fecdc9ae29c63f
Fatema Mouri <fatema.mouri@dcmail.ca>
2022-01-21 19:46:56 -0500

    updated ICE-2

--- Scripts/app.js
+++ Scripts/app.js
@@ -10,6 +10,49 @@
         {
             location.href = "about.html";
         });
+
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;        
+
+        // Step 2 - Create a HTML Element in memory
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagragh = `<p id="ArticleParagraph" class="mt-3">This is the Article Paragraph</p>`
+
+
+        // Step 3 - Congiure new Element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        //MainParagraph.textContent = "This is the Main Paragragh!";
+        let FirstString = "This is"
+        let SecondString = `${FirstString} the main Paragraph`;
+        MainParagraph.textContent = SecondString;
+        Article.setAttribute("class","container");
+
+
+        // Step 4 - perform insertion / deletion
+
+        // example of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagragh;
+        DocumentBody.appendChild(Article);
+        // DocumentBody.innerHTML =`
+        //     <div class="container">
+        //     <h1 class="display-1">Hello, World!</h1>
+        //     <p class="mt-5">and ... I think this is an awesome class</p>
+        //     </div>
+        // `;
+
+        // example of insert before
+        //MainContent.before(MainParagraph);
+
+        // example of deletion
+        //document.getElementById("AboutUsButton").remove();
+        //AboutUsButton.remove();
+
+        // ES6 and HTML5 => Template Strings => "Super Strings"
+        
     }
 
     function DisplayProductsPage()
